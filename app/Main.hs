940434c8d3a0c365213ea3a60725e9dd153770d0
Andrew Stanton <refefer@gmail.com>
2015-12-07 11:55:16 -0800

    Adds commandline opts

--- app/Main.hs
+++ app/Main.hs
@@ -8,13 +8,25 @@ import Network.Wai (responseLBS, rawPathInfo, Application)
 import Network.Wai.Handler.Warp (run)
 import Network.HTTP.Types (status200, status404)
 import Network.HTTP.Types.Header (hContentType)
+import Options.Applicative
+
 import Lib
 
-main = do
-    let port = 3000
-    putStrLn $ "Listening on port " ++ show port
-    let fs = newFS "/tmp/cached"
-    run port (app fs)
+data Settings = Settings { port :: Int
+                         , path :: String 
+                         }
+
+settings :: Parser Settings 
+settings = Settings 
+  <$> option auto
+    ( long "port"
+   <> short 'p'
+   <> help "Port to run fido on" )
+  <*> strOption
+    ( long "path"
+   <> short 'd' 
+   <> metavar "DIRECTORY" 
+   <> help "Path to the directory for storing the files" )
 
 app :: FileSystem -> Application
 app fs req respond = do
@@ -23,4 +35,16 @@ app fs req respond = do
     respond $ case inf of
       Just lbs -> responseLBS status200 [(hContentType, "image/jpeg")] lbs 
       Nothing  -> responseLBS status404 [(hContentType, "text/plain")] "404 - Not Found" 
+
+main = do
+    s <- execParser opts
+    let p = port s
+    putStrLn $ "Listening on port " ++ show p
+    let fs = newFS . path $s
+    run p (app fs)
+  where
+    opts = info (helper <*> settings)
+      ( fullDesc
+     <> progDesc "Runs a server that proxies and caches images to disk"
+     <> progDesc "fido - fetches files from a server" )
 
